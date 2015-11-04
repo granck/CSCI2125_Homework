89d559a3c0127bf9a9ce34a6efc4cf9cb886fc52
Garrick Linden Ranck <granck@math320-m07.cs.uno.edu>
2015-11-04 13:32:22 -0600

    Fixed insert(value) and implemented insert(node, value). Added empty remove method.

--- Assignment2/BinarySearchTree.java
+++ Assignment2/BinarySearchTree.java
@@ -7,36 +7,38 @@
 
 public class BinarySearchTree<T> extends BinaryTree<T>{
    
-   BinaryNode<T> rootPtr; //holds root of a specific tree
    private int numOfNodes = 0;
+	
+	public BinarySearchTree(){
+		super();
+	}//end consructor
 
    public BinarySearchTree(T[] seq){
       super(seq);
-
    }//end constructor
 
    public BinarySearchTree(T[] seq, T nullSymbol){
       super(seq, nullSymbol);
    }//end constructor
    
-   //recursive
    //inserts value at appropriate location in tree
    public void insert(T value){
       if (root.getData() == null)
          root.setData(value);   
       
-      //else if value is less than true root
-      else if(value.compareTo(root.getData())){
+      //determine if value is less than root
+      else if(value.compareTo(root.getData()) < 0){
          //determine if root has left child
-         if(root.getLeftNode())
+         if(root.getLeftNode() != null)
             insert(root.getLeftNode(), value);
          else
             root.setLeftNode(new BinaryNode<T>(value));
       }//end else if
 
-      //else value is greater than root
-      else{ 
-         if(root.getRightNode())
+      //determine if value is greater than root
+		else if(value.compareTo(root.getData()) > 0){ 
+			//determine if root has right child
+         if(root.getRightNode() != null)
             insert(root.getRightNode(), value);
          else
             root.setRightNode(new BinaryNode<T>(value));
@@ -44,11 +46,36 @@ public class BinarySearchTree<T> extends BinaryTree<T>{
 
    }//end method insert
 
-   //inserts value at appropriate location in tree
-   //overloaded. Accepts Child of previous node and value to insert
+   //recursively determines placement of value in tree
+	//does nothing if value currently exists in tree
+	//inserts value at appropriate location in tree
+   //accepts child of previous node and value to insert
    public void insert(BinaryNode<T> currentRoot, T value){
+		
+		//determine if value is less than root 
+		if(value.compareTo(currentRoot.getData()) < 0){
+			//deterine if root has a left child
+			if(root.getLeftNode() != null)
+				insert(root.getLeftNode(), value);
+			else
+				root.setLeftNode(new BinaryNode<T>(value));
+		}//end if
+		
+		//determine if value is greater than root
+		else if(value.compareTo(currentRoot.getData()) > 0){
+			//determine if root has a right child
+			if(root.getRightNode() != null)
+				insert(root.getRightNode(), value);
+			else
+				root.setRightNode(new BinaryNode<T>(value));
+		}//end else if
 
    }//end method insert overloaded
+	
+	//removes node 
+	public void remove(T value){
+
+	}//end method remove
 
    public int getSize(){
       return numOfNodes;
