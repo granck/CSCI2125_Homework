97e92d2767dc3fe0175a92967e43041e59300b07
Garrick Ranck <granck@uno.edu>
2015-11-05 13:42:06 -0600

    Tests methods as I complete them in classes.

---
+++ Assignment2/SimpleTest.java
@@ -0,0 +1,26 @@
+public class SimpleTest{
+
+   public static void main(String[] args){
+
+      final Integer nullSymbol = 0;
+
+      Integer[] breadthFirstSequence = {27, 13, 59, 3, 17, 43, 107, 2, 11};
+
+      System.out.println("Using default breadth-first traversal breadthFirstSequence = "
+            + java.util.Arrays.toString(breadthFirstSequence));
+
+      BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>(breadthFirstSequence, nullSymbol);
+ 
+		System.out.println("Height = " + tree.height());
+		System.out.println("Width = " + tree.width());
+		System.out.println("preOrderTraverse: " + tree.preOrderTraverse());		
+		System.out.println("postOrderTraverse: " + tree.postOrderTraverse());		
+		System.out.println("inOrderTraverse: " + tree.inOrderTraverse());
+		System.out.println("breadthFirstTraverse: " + tree.breadthFirstTraverse());
+      
+      tree.insert(5);
+      System.out.println("inOrderTraversal after adding 5: " + tree.inOrderTraverse());
+
+
+   }//end main method
+}//end class
