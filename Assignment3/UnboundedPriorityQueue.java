c01b10469118013c236cfe412ef9032c00c1195c
Garrick Ranck <granck@uno.edu>
2015-11-13 14:49:31 -0600

    Empty implementation of UnboundPriorityQueue.

---
+++ Assignment3/UnboundedPriorityQueue.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class UnboundedPriorityQueue<T extends Comparable<T>> {
+
+   ArrayList<T> que;
+   
+   public UnboundedPriorityQueue(){
+      que = new ArrayList<T>(); 
+
+   }//end constructor 
+
+   public void enqueue(T task){
+
+
+   }//end method enqueue
+
+   public T dequeue(){
+      return;
+   }//end method dequeue
+
+
+   public boolean isEmpty(){
+      return true;
+
+
+   }//end method isEmpty
+
+}//end class UnboundedPriorityQue
