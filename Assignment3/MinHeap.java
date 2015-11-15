547adef22bd3cf925515319cd04b3b278f4cb386
Garrick Linden Ranck <granck@uno.edu>
2015-11-15 17:17:43 -0600

    implemented remove method and bubble down feature. Still needs testing along with rest of code.

--- Assignment3/MinHeap.java
+++ Assignment3/MinHeap.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 
 public class MinHeap<T extends Comparable<T>>{
-   
+
    private ArrayList<T> heap;
 
    public MinHeap(){
       heap = new ArrayList<T>();   
 
    }//end constructor
-   
+
    //@param task of type T, task that is to be inserted into the heap
    public void insert(T task){
       int index = heap.size(); //size of heap, equal to index of element added
       heap.add(task);
-      
+
       //while parent is greater than child perform swap 
       while(heap.get((index - 1)/2).compareTo(heap.get(index)) > 0){
          T replaced = heap.get((index - 1)/2); //temp value to store parent element
@@ -25,26 +25,68 @@ public class MinHeap<T extends Comparable<T>>{
 
 
    }//end end method insert
-   
-   //@returns task of type T, task that was removed from heap 
+   /*
+    * removes first value in heap and replaces it with last value in heap
+    * then bubbles replaced value down heap until it is in proper location again
+    *
+    * @returns task of type T, task that was removed from heap 
+    **/
    public T remove(){
       T replaced = heap.get(heap.size() - 1);
       T removed = heap.get(0);
-      heap.remove(0);
       heap.set(0, replaced);
       heap.remove(heap.size() - 1);
-      return removed; //still need to bubble down new root to proper index 
+      int index = 0;
 
+      while(true){
+
+         //if left child is less than parent
+         if((index*2 + 1) < heap.size() && heap.get(index*2 + 1).compareTo(heap.get(index)) < 0){
+
+            //if right child is less than left child
+            //by transitive property, right child will be less than parent as well
+            if((index*2 + 2) < heap.size() && heap.get(index*2 + 2).compareTo(heap.get(index* 2 + 1)) < 0 ){
+               T swap = heap.get(index*2 + 2); //stores value in child index
+               heap.set((index*2 + 2), heap.get(index)); //sets child index to parent value
+               heap.set(index, swap); //sets parent index to child value
+               index = index*2 + 2; //increment index to location of child bubbled down
+            }//end inner if
+            
+            //else if 
+            else{ 
+            T swap = heap.get(index*2 + 1); //stores value in child index
+            heap.set((index*2 + 1), heap.get(index)); //sets child index to parent value
+            heap.set(index, swap); //sets parent index to child value
+            index = index*2 + 1; //increment index to location of child bubbled down
+            }// end else
+
+         }//end outer if
+         
+         //else if left child isn't less, check right child seperately
+         else if((index*2 + 2) < heap.size() && heap.get(index*2 + 2).compareTo(heap.get(index)) < 0 ){
+               T swap = heap.get(index*2 + 2); //stores value in child index
+               heap.set((index*2 + 2), heap.get(index)); //sets child index to parent value
+               heap.set(index, swap); //sets parent index to child value
+               index = index*2 + 2; //increment index to location of child bubbled down
+
+         }//end else if
+
+         //else if neither left or right child is less, break loop
+         else
+            break;
+
+      }//end while
+
+      return removed; 
 
    }//end method remove
 
-   
    //@returns true if heap is empty, false if not
    public boolean isEmpty(){
       return heap.isEmpty();
 
    }//end method isEmpty
-   
+
 
    //@returns true if heap is full, false if not
    public boolean isFull(){
@@ -53,7 +95,7 @@ public class MinHeap<T extends Comparable<T>>{
    }//end method isFull
 
 
-  
+
 
 
 
