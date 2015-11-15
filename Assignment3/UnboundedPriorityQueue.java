f494e36fb30ee9d9b574ed27cbf0e551e27b5d04
Garrick Linden Ranck <granck@uno.edu>
2015-11-15 15:19:31 -0600

    moved implementation of priority que into minheap class. Should be fully fleshed.

--- Assignment3/UnboundedPriorityQueue.java
+++ Assignment3/UnboundedPriorityQueue.java
@@ -1,27 +1,31 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class UnboundedPriorityQueue<T extends Comparable<T>> {
 
-   ArrayList<T> que;
-   
+   MinHeap<T> que; 
    public UnboundedPriorityQueue(){
-      que = new ArrayList<T>(); 
+      que = new MinHeap<T>(); 
 
    }//end constructor 
+   
 
+   //adds task to end of arraylist
+   //bubble up until in proper index 
    public void enqueue(T task){
-
+      que.insert(task); //still need to bubble element up to proper index
 
    }//end method enqueue
-
+   
+   //removes first index from arraylist
+   //places last index into first
+   //bubble down until in proper index
    public T dequeue(){
-      return;
+      return que.remove();
    }//end method dequeue
 
 
    public boolean isEmpty(){
-      return true;
-
+      return que.isEmpty();
 
    }//end method isEmpty
 
