943eef110be1c4cdf724cce8550c7d6d6e5bd87f
Garrick Ranck <granck@uno.edu>
2015-11-20 14:50:49 -0600

    Stuff

--- Assignment3/UnboundedPriorityQueueTest.java
+++ Assignment3/UnboundedPriorityQueueTest.java
@@ -1,23 +1,23 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class UnboundPriorityQueueTest{
+public class UnboundedPriorityQueueTest{
 	
 	@Test
 	public void TestEmptyQueueIsEmpty(){
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
-		AssertEquals(true, taskQueue.isEmpty());
-		AssertEquals(0, taskQueue.getSize());
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
+		assertEquals(true, taskQueue.isEmpty());
+		assertEquals(0, taskQueue.getSize());
 	
 	}//end method TestEmptyQueueIsEmpty
 	@Test
 	public void TestPushWithEmptyQueue(){
 		Task task1 = new Task("Task1", 1230, 20);
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
 
 		taskQueue.enqueue(task1);
-		AssertEquals(false, taskQueue.isEmpty());
-		AssertEquals(1, taskQueue.getSize());
+		assertEquals(false, taskQueue.isEmpty());
+		assertEquals(1, taskQueue.getSize());
 
 	}//end method TestPushWithEmptyQueue
 
@@ -25,15 +25,15 @@ public class UnboundPriorityQueueTest{
 	public void TestPushWithNonEmptyQueue(){
 		Task task1 = new Task("Task1", 1230, 20);
 		Task task2 = new Task("Task2", 1330, 10);
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
 
 		taskQueue.enqueue(task1);
-		AssertEquals(false, taskQueue.isEmpty());
-		AssertEquals(1, taskQueue.getSize());
+		assertEquals(false, taskQueue.isEmpty());
+		assertEquals(1, taskQueue.getSize());
 
 		taskQueue.enqueue(task2);
-		AssertEquals(false, taskQueue.isEmpty());
-		AssertEquals(2, taskQueue.getSize());
+		assertEquals(false, taskQueue.isEmpty());
+		assertEquals(2, taskQueue.getSize());
 		
 	}//end method TestPushWithNonEmptyQueue
 
@@ -41,47 +41,46 @@ public class UnboundPriorityQueueTest{
 	public void TestPushWithValueAlreadyInQueue(){
 		Task task1 = new Task("Task1", 1330, 10);
 		Task task2 = new Task("Task2", 1330, 10);
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
 
 		taskQueue.enqueue(task1);
-		AssertEquals(false, taskQueue.isEmpty());
-		AssertEquals(1, taskQueue.getSize());
+		assertEquals(false, taskQueue.isEmpty());
+		assertEquals(1, taskQueue.getSize());
 
 		taskQueue.enqueue(task2);
-		AssertEquals(false, taskQueue.isEmpty());
-		AssertEquals(2, taskQueue.getSize());
+		assertEquals(false, taskQueue.isEmpty());
+		assertEquals(2, taskQueue.getSize());
 
 	}//end method TestPushWithValueAlreadyInQueue
 
 	@Test
 	public void TestPopWithEmptyQueue(){
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
-		AssertEquals(null, taskQueue.dequeue());
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
+		assertEquals(null, taskQueue.dequeue());
 
 	}//end method TestPopWithEmptyQueue
 
 	@Test
 	public void TestPopWithNonEmptyQueue(){
 		Task task1 = new Task("Task1", 1330, 10);
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
 
 		taskQueue.enqueue(task1);
-		AssertEquals("Task1", taskQueue.dequeue());
+		assertEquals("Task1", taskQueue.dequeue());
 	
 	}//end method TestPopWithNonEmptyQueue
 
 	@Test
 	public void TestPopBeforeAndAfterEmptyQueue(){
 		Task task1 = new Task("Task1", 1330, 10);
-		UnboundPriorityQueue<Task> taskQueue = new UnboundPriorityQueue<Task>();
+		UnboundedPriorityQueue<Task> taskQueue = new UnboundedPriorityQueue<Task>();
 
 		taskQueue.enqueue(task1);
-		AssertEquals("Task1", taskQueue.dequeue());
-		AssertEquals(null, taskQueue.dequeue());
-			
+		assertEquals("Task1", taskQueue.dequeue());
+		assertEquals(null, taskQueue.dequeue());
 
 	}//end method TestPopBeforeAndAfterEmptyQueue
 
 
 
-//end UnboundPriorityQueueTest class
+}//end UnboundedPriorityQueueTest class
