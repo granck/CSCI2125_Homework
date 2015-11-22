12129a5e0bd4496dec6ec6db5f95651d02ee082a
Garrick Linden Ranck <granck@uno.edu>
2015-11-21 18:01:26 -0600

    Cleaned up code. Altered parameter of while loop in remove method.

--- Assignment3/UnboundedPriorityQueue.java
+++ Assignment3/UnboundedPriorityQueue.java
@@ -11,6 +11,7 @@ public class UnboundedPriorityQueue<T extends Comparable<T>> {
 
    //adds task to end of arraylist
    //bubble up until in proper index 
+	//@parameter value of type T, passes value into que
    public void enqueue(T task){
       que.insert(task); //still need to bubble element up to proper index
 
@@ -19,16 +20,18 @@ public class UnboundedPriorityQueue<T extends Comparable<T>> {
    //removes first index from arraylist
    //places last index into first
    //bubble down until in proper index
+	//@preturns first element of type T in heap
    public T dequeue(){
       return que.remove();
    }//end method dequeue
 
-
+	//@returns true if heap is empty, else returns false
    public boolean isEmpty(){
       return que.isEmpty();
 
    }//end method isEmpty
-
+	
+	//@returns integer number of elements in heap
 	public int getSize(){
 		return que.getSize();
 	}//end method getSize
